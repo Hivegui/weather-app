9b53b9c6bfd77a2054259485fa86921780e106cd
Guilherme Gomes <guifariasgomes@gmail.com>
2024-06-04 12:25:43 -0300

    primeiro commit weather-map
    primeiro commit

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private apiKey = 'e052df71ce7ea4e6e933f17456b89270';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
+      {}
+    );
+  }
+}
